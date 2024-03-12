c42f2d5f4075db60876ab00426a0613ccbe3c205
Alyona <109202342+newfolder2@users.noreply.github.com>
2024-03-12 15:10:09 +0700

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+  $HOST="localhost";
+  $USER="pehopil362";
+  $PASSWORD="123456789Aa";
+  $DB="pehopil362";
+  $connection = new mysqli ($HOST, $USER, $PASSWORD, $DB);
+  $connection -> query ('SET NAMES "utf8"');
+?>
